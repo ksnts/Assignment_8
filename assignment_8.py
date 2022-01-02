f8177f90c678506631cda03566f69e29bce908f4
ksnts <santosjohnkyllem@gmail.com>
2022-01-02 22:59:23 +0800

    First draft

---
+++ assignment_8.py
@@ -0,0 +1,19 @@
+from random import randrange
+
+x=1
+
+while x == 1:
+
+    numr1=randrange(0,9)
+    numr2=randrange(0,9)
+    numr3=randrange(0,9)
+
+    num1=int(input("Enter first number: "))
+    num2=int(input("Enter second number: "))
+    num3=int(input("Enter third number: "))
+
+    lottoN= [numr1, numr2, numr3]
+    
+
+
+
